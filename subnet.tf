b0d4cf97a89f7dfbb35695e85de2e4c5bf0c501a
Kreyno93 <michael.wagener@neuefische.de>
2026-04-10 11:46:10 +0200

    adding second - private - subnet for later use (rds)

--- subnet.tf
+++ subnet.tf
@@ -6,3 +6,12 @@ resource "aws_subnet" "public_subnet" {
     Name = "Public-Subnet"
   }
 }
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.wordpress_vpc.id # Reference
+  cidr_block = "172.16.0.64/26"
+
+  tags = {
+    Name = "Private-Subnet"
+  }
+}
