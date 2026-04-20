d07237926bbe5903803fc02ba93b075ce2edde02
Kreyno93 <michael.wagener@neuefische.de>
2026-04-20 14:47:45 +0200

    adding new Bastion SG ; and adjust the SG for the Bastion Host Instance

--- ec2.tf
+++ ec2.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "bastion-host" {
   instance_type               = "t3.micro"
   associate_public_ip_address = true
   key_name                    = "vockey"
-  vpc_security_group_ids      = [aws_security_group.wordpress-sg.id]
+  vpc_security_group_ids      = [aws_security_group.bastion-sg.id]
   subnet_id                   = aws_subnet.public_subnet.id
 
   tags = {
--- sg.tf
+++ sg.tf
@@ -42,3 +42,26 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_ssh" {
   ip_protocol       = "-1"
 }
 
+resource "aws_security_group" "bastion-sg" {
+  name        = "bastion-sg"
+  description = "Allow SSH traffic"
+  vpc_id      = aws_vpc.wordpress_vpc.id
+  tags = {
+    Name = "bastion-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "bastion-ssh" {
+  security_group_id = aws_security_group.bastion-sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_ssh" {
+  security_group_id = aws_security_group.bastion-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
