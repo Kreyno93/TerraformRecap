80f0087bbe77cccf906691fd39bc849e9f6af303
Kreyno93 <michael.wagener@neuefische.de>
2026-04-20 14:41:27 +0200

    Adding a bastion host to act like a jump box

--- ec2.tf
+++ ec2.tf
@@ -10,3 +10,16 @@ resource "aws_instance" "wordpress_instance" {
     Name = "Wordpress-Instance"
   }
 }
+
+resource "aws_instance" "bastion-host" {
+  ami                         = "ami-0a914de4dc1f18727"
+  instance_type               = "t3.micro"
+  associate_public_ip_address = true
+  key_name                    = "vockey"
+  vpc_security_group_ids      = [aws_security_group.wordpress-sg.id]
+  subnet_id                   = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "Bastion-Host"
+  }
+}
